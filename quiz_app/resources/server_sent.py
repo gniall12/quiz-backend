a82496f1fb1aa6644bf136c08a1d047c3cd52ab7
Niall Gallagher <gniall12@gmail.com>
2020-06-30 21:18:45 +0100

    Revert "Added docstrings to server sent resource"
    This reverts commit ff93771d45bad6410c0f88d22c584f67cb1b5cab.

--- quiz_app/resources/server_sent.py
+++ quiz_app/resources/server_sent.py
@@ -6,16 +6,5 @@ from flask_sse import sse
 
 class Change(Resource):
     def get(self, quiz_id):
-        """Publishes message to client to notify them of a change in quiz state.
-
-        Used to let client know when the quizmaster has progressed to the 
-        next stage or round of the quiz.
-
-        Args:
-            quiz_id: The ID of the quiz that is progressing.
-
-        Returns:
-            The quiz ID of the quiz that is progressing.
-        """
         sse.publish({"message": "Hello!"}, channel=quiz_id)
         return quiz_id
